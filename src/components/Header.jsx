978eb4c3fc5cadf6bce232ef28c6cac177864275
katesilatory <katesilatory@gmail.com>
2023-01-18 17:14:51 +0800

    Updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     return (
     <div className={styles.container}>
       <div>
-        {/* <a><img className={styles.logo} src="./images/coet.jpg" alt="logo"/></a> */}
+        {/* <a><img className={styles.logo} src="./images/coet.jpg" alt="logo"/></a>  */}
       </div>
       <div>
         <a href="/" className={styles.buttontext} ><button className={styles.primary}>Logout &nbsp;<LogoutIcon sx={{ fontSize: 20 }} /></button></a>
